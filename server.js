b8f3c1dab15f0db3fe8048532db4b14d1402686a
Joseph Chatelin <josephchatelin@gmail.com>
2019-09-25 10:44:03 +0200

    launch HTTP server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+var http = require("http");
+
+var server  = http.createServer(function(req, res){
+    res.writeHead(200);
+    res.end("Hello world!");
+}).listen(3000, "127.0.0.1");
