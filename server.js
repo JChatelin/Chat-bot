d3eb291328d1dc24dfed24f61d4952abf79e6714
Joseph Chatelin <josephchatelin@gmail.com>
2019-09-25 12:48:04 +0200

    add a paramater to the  url

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 
-app.get('/', function(req, res) {
-    res.send("Hello World!")
+app.get('/hello:name?', function(req, res) {
+    if (req.query.name) {
+        res.send("Hello " + req.query.name + "!\n")
+    } else {
+        res.send("What is your name?\n");
+    }
 })
 
 app.listen(process.env.PORT || 3000, function() {
